9d7cd391a3d62c4970c0e6eda65c8a740e837b1a
M.Polverini <m.polverini@almaviva.it>
2021-10-23 11:32:00 +0200

    implemented call with axios/cheerio

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import axios, { AxiosResponse } from 'axios';
+import { AxiosResponse } from 'axios';
 import cheerio from 'cheerio';
+import { HttpService } from '@nestjs/axios';
+import { map, Observable } from 'rxjs';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private httpService: HttpService,
+  ) {}
 
   @Get('/news')
-  getNews(): { title: string; url: string }[] {
-    axios
-      .get('https://www.corrieredellosport.it/')
-      .then((response: AxiosResponse<string>) => {
-        console.log(response.data);
-        let $ = cheerio.load(response.data);
-        console.log($);
-      });
-    return [];
+  getNews(): Observable<{ title: string; url: string }[]> {
+    let result: { title: string; url: string }[] = [];
+    let string = 'loading';
+    return this.httpService.get('https://www.corrieredellosport.it/').pipe(
+      map((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        $('a:contains("roma")', html).each(function () {
+          const title = $(this).text();
+          const url = $(this).attr('href');
+          result.push({ title, url });
+        });
+        return result;
+      }),
+    );
   }
 }
